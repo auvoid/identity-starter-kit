fce1b1e6fc4ce32c12ceebe781e02d4360532a24
Soham Jaiswal <sosweetham@gmail.com>
2024-10-23 18:17:04 +0530

    fix: add missing files

---
+++ apps/client/src/lib/components/ui/Button.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte';
+	import { Button } from 'flowbite-svelte';
+	import { twMerge } from 'tailwind-merge';
+
+	interface $$Props extends Omit<ComponentProps<Button>, 'color' | 'buttonClass'> {
+		color?: 'primary' | 'alternative' | 'none';
+		buttonClass?: string;
+	}
+
+	export let color: $$Props['color'] = 'primary';
+	export let buttonClass = '';
+	switch (color) {
+		case 'primary':
+			buttonClass += ' bg-brand-yellow text-gray-800 hover:bg-brand-yellow_hover focus:ring-0';
+			break;
+		case 'alternative':
+			buttonClass +=
+				' dark:bg-gray-800 bg-gray-50 text-gray-800 dark:text-gray-400 border-[1px] dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-0 border-gray-300';
+			break;
+		default:
+			buttonClass += '';
+			break;
+	}
+</script>
+
+<Button
+	class={twMerge(buttonClass, $$restProps.class)}
+	{...$$restProps}
+	on:click
+	on:change
+	on:keydown
+	on:keyup
+	on:touchend
+	on:touchcancel
+	on:mouseenter
+	on:mouseleave
+>
+	<slot />
+</Button>
---
+++ apps/client/src/routes/(auth)/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Card } from 'flowbite-svelte';
+</script>
+
+<div class="flex h-full w-full items-center justify-center bg-gray-900">
+	<Card>
+		<div class="max-w-[500px]">
+			<slot />
+		</div>
+	</Card>
+</div>
