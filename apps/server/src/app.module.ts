7832328da80e45c23c756507055361db56672a35
Merul Dhiman <69296233+coodos@users.noreply.github.com>
2024-10-24 23:30:22 +0800

    Feat/backend db initiation (#18)
    * chore: fucking migration bullshit with typeorm
    
    * chore: push user module stuff

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './db/data-source';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
   controllers: [],
   providers: [],
 })
